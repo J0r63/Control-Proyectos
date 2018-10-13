9b9d44bdb1f91eec47adda47681b364ae2cc1b66
J0r63 <thewall+J0r63@users.noreply.github.com>
2018-10-13 00:34:28 -0600

    modulo_admin

---
+++ sistema/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Brought To You By <a href="http://www.codigofuentegratis.net/">codigo fuente gratis</a></b>
+    </div>
+    <strong>Copyright &copy; 2018 codigofuentegratis </strong>
+</footer>
