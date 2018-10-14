660c7779c474010f460b9486ee0ee5bf2a16dd35
J0r63 <thewall+J0r63@users.noreply.github.com>
2018-10-13 22:22:23 -0600

    footer
    cambios

--- sistema/admin/includes/footer.php
+++ sistema/admin/includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Brought To You By <a href="http://www.codigofuentegratis.net/">codigo fuente gratis</a></b>
+      <b>Brought To You By <a href="http://www.codigofuentegratis.net/"></a></b>
     </div>
-    <strong>Copyright &copy; 2018 codigofuentegratis </strong>
+    <strong>Copyright &copy; 2018 </strong>
 </footer>
